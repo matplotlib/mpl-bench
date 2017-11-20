1869dcba7c1b2805ddc29a74c368bd01a677dd1a
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2017-11-20 03:58:18 -0500

    Add some simple benchmarks.

---
+++ benchmarks/basic.py
@@ -0,0 +1,33 @@
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+
+import matplotlib.pyplot as plt
+
+
+def teardown(*args, **kwargs):
+    plt.close('all')
+
+
+def time_subplots(N):
+    fig, ax = plt.subplots(N, N)
+    fig.canvas.draw()
+time_subplots.params = (1, 2, 10)
+time_subplots.param_names = ('N', )
+
+
+def time_projection(proj):
+    fig, ax = plt.subplots(subplot_kw=dict(projection=proj))
+    fig.canvas.draw()
+time_projection.params = ('polar', 'aitoff', 'hammer', 'mollweide', 'lambert')
+time_projection.param_names = ('proj', )
+
+
+def time_plot():
+    fig, ax = plt.subplots()
+    ax.plot([0, 1, 2], [0, 1, 2])
+    fig.canvas.draw()
+
+
+def time_savefig():
+    fig, ax = plt.subplots()
+    fig.savefig('time_savefig.png')
